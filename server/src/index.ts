280e64186a0c7b14f4e7475d0ae4a490e2f7503e
이효진 <rabbit3456@naver.com>
2025-07-29 17:20:46 +0900

    build: DB연결

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import router from './routes/test'
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use("/",router);
 
 app.get("/", (_, res) => {
 	res.send("GenAI 개발일지 백엔드 작동중!");
