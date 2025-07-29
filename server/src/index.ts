da10b59754d9608bd39dd500db5098f11d876663
이효진 <rabbit3456@naver.com>
2025-07-29 20:31:37 +0900

    feat: cors 설정

--- server/src/index.ts
+++ server/src/index.ts
@@ -6,7 +6,11 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: '*' }))
+/* app.use(cors({
+  	origin: 'https://genai-dev-journal.vercel.app/', // 실제 Vercel 주소
+	credentials: true,
+})) */
 app.use(express.json());
 
 app.use("/",router);
