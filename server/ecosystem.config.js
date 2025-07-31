100eb2ee8b36fb07a655bfd38a616e591032a81a
이효진 <rabbit3456@naver.com>
2025-08-01 01:07:46 +0900

    fix: ecosystem.config.js 수정

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,13 +1,26 @@
+// ecosystem.config.js
 module.exports = {
 	apps: [
 		{
-			name: "genai-backend",
+			name: "genai-backend-development",
 			script: "./dist/index.js",
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: "1G",
 			env: {
-				NODE_ENV: "development",
+				NODE_ENV: "development", // 개발용 환경
 			},
-			env_production: {
-				NODE_ENV: "production",
+		},
+		{
+			name: "genai-backend-production",
+			script: "./dist/index.js",
+			instances: "max",
+			autorestart: true,
+			watch: false,
+			max_memory_restart: "1G",
+			env: {
+				NODE_ENV: "production", // 운영용 환경
 			},
 		},
 	],
