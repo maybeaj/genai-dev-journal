efe3fb04cc3668430bc84ffbfb8bb410ed8ffd33
이효진 <rabbit3456@naver.com>
2025-08-01 10:09:50 +0900

    chore: env 경로 설정 중

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv";
 
-const mode = process.env.NODE_ENV || "development";
-const envPath = mode === "production" ? ".env.production" : ".env";
+const envPath = process.env.NODE_ENV === "production" ? ".env.production" : ".env";
 dotenv.config({ path: envPath });
 
 import express from "express";
