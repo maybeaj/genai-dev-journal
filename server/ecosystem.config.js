e9b0d77c39bca1f3fb7ba0b140b2a417d156c870
이효진 <rabbit3456@naver.com>
2025-08-01 10:39:52 +0900

    chore: ecosystem.config.js 수정

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -1,8 +1,16 @@
+// ecosystem.config.js
 module.exports = {
 	apps: [
 		{
 			name: "genai-backend",
 			script: "./dist/index.js",
+			instances: 1,
+			autorestart: true,
+			watch: false,
+			max_memory_restart: "1G",
+			env: {
+				NODE_ENV: "development",
+			},
 			env_production: {
 				NODE_ENV: "production",
 			},
